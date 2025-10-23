4cd30c9bcd85155825bdbda3a569c8f72980a999
Dan F <dpdfoley@yahoo.com>
2025-10-23 11:30:08 -0500

    Update todo list styling and fix button functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,20 @@
 const deleteBtn = document.querySelectorAll('.del')
 const todoItem = document.querySelectorAll('span.not')
 const todoComplete = document.querySelectorAll('span.completed')
-const editBtns = document.querySelectorAll('span.edit');
+const editBtns = document.querySelectorAll('.edit');
 const completedTasks = document.getElementById('completedTasks');  // Get the completed tasks element
 const prioritySelects = document.querySelectorAll('.priority-select');
 
-Array.from(deleteBtn).forEach((el)=>{  
+// Event listeners
+Array.from(deleteBtn).forEach((el) => {  
     el.addEventListener('click', deleteTodo)
 })
 
-Array.from(todoItem).forEach((el)=>{
+Array.from(todoItem).forEach((el) => {
     el.addEventListener('click', markComplete)
 })
 
-Array.from(todoComplete).forEach((el)=>{
+Array.from(todoComplete).forEach((el) => {
     el.addEventListener('click', markIncomplete)
 })
 
@@ -21,62 +22,63 @@ Array.from(editBtns).forEach((el) => {
     el.addEventListener('click', editTodo); 
 });
 
+// Delete todo
 async function deleteTodo(){
-    const todoId = this.parentNode.dataset.id
-    try{
+    const li = this.closest('li'); // get the <li> element
+    const todoId = li.dataset.id;
+    try {
         const response = await fetch('todos/deleteTodo', {
             method: 'delete',
             headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'todoIdFromJSFile': todoId
-            })
-        })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
+            body: JSON.stringify({ 'todoIdFromJSFile': todoId })
+        });
+        const data = await response.json();
+        console.log(data);
+        location.reload();
+    } catch(err) {
+        console.log(err);
     }
 }
 
+// Mark as complete
 async function markComplete(){
-    const todoId = this.parentNode.dataset.id
-    try{
+    const li = this.closest('li');
+    const todoId = li.dataset.id;
+    try {
         const response = await fetch('todos/markComplete', {
             method: 'put',
             headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'todoIdFromJSFile': todoId
-            })
-        })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
+            body: JSON.stringify({ 'todoIdFromJSFile': todoId })
+        });
+        const data = await response.json();
+        console.log(data);
+        location.reload();
+    } catch(err) {
+        console.log(err);
     }
 }
 
+// Mark as incomplete
 async function markIncomplete(){
-    const todoId = this.parentNode.dataset.id
-    try{
+    const li = this.closest('li');
+    const todoId = li.dataset.id;
+    try {
         const response = await fetch('todos/markIncomplete', {
             method: 'put',
             headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'todoIdFromJSFile': todoId
-            })
-        })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
+            body: JSON.stringify({ 'todoIdFromJSFile': todoId })
+        });
+        const data = await response.json();
+        console.log(data);
+        location.reload();
+    } catch(err) {
+        console.log(err);
     }
 }
 
+// Edit todo
 async function editTodo() {
-    const li = this.parentNode;
+    const li = this.closest('li');  // fixed to get the <li>
     const span = li.querySelector('span.not, span.completed'); 
     const oldText = span.innerText;
 
@@ -115,7 +117,7 @@ async function editTodo() {
     });
 }
 
-// Update message when all todos are completed
+// Update completed message
 function updateMessage(){
     const todos = document.querySelectorAll('span.not, span.completed');
     const completed = document.querySelectorAll('span.completed');
@@ -130,6 +132,7 @@ function updateMessage(){
 }
 updateMessage();
 
+// Priority change
 prioritySelects.forEach(sel => sel.addEventListener('change', async function() {
     const li = this.closest('li');
     const todoId = li.dataset.id;
